1fb05de8dc30dedc69d4d7c2ae2d43dc288ac3b1
Jorge Gomez <jgomezmail-dev@proton.me>
2026-08-29 11:42:39 -0400

    adding initial flake and devshell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "AI Flow Devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              claude-code
+              openspec
+              gh
+              git
+              ctx7
+              bubblewrap
+            ];
+          };
+        };
+    };
+
+}
